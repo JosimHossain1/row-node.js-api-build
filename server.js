ae00d191455ddd7e0fdb6f00c633823f1ee2c968
JosimHossain1 <josimhossain842@gmail.com>
2025-12-10 08:38:17 +0600

    Create basic server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World 2');
+});
+
+server.listen(port, () => {
+  console.log('listening in port', port);
+});
